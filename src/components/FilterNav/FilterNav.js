25c995af8e7911b5fe7d6f3fd16da3f48db46fbc
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-27 23:33:09 -0500

    attach events to buttons

--- src/components/FilterNav/FilterNav.js
+++ src/components/FilterNav/FilterNav.js
@@ -1,20 +1,41 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import Search from '../assets/imgs/Search.png'
 import Filter from '../assets/imgs/Filter.png'
 import Arrow from '../assets/imgs/Arrow.png'
 import { motion } from "framer-motion";
 import { dropdownAnimate } from '../../animations/FilterNav';
 import CheckboxInput from './CheckboxInput/CheckboxInput';
+import { MoviesContext } from '../../contextProviders/MoviesProvider';
 
 const FilterNav = ({handleSubmit, genres}) => {
     const [isHover, toggleHover] = useState(false)
+    const [state, dispatch] = useContext(MoviesContext);
     const inputRef = useRef()
     const toggleMenu = () => {
         inputRef.current.focus()
         toggleHover(!isHover)
     }
+    const handleASCBtn = () => {
+        dispatch({
+            type: 'SORT_MOVIES_ASC',
+        });
+    };
+    const handleDESBtn = () => {
+        dispatch({
+            type: 'SORT_MOVIES_DES',
+        });
+    };
+    const handle10_0Btn = () => {
+        dispatch({
+            type: 'SORT_10-0',
+        });
+    };
+    const handle0_10Btn = () => {
+        dispatch({
+            type: 'SORT_0-10',
+        });
+    };
     return (
-        
         <nav>
             <div className="search">
                 <input type="Text" />
@@ -60,11 +81,11 @@ const FilterNav = ({handleSubmit, genres}) => {
                     <div className="sub-menu-background" />
                     <div className="sub-menu-container">
                         <h2>Fecha</h2>
-                        <button>Nuevas - Antiguas</button>
-                        <button>Antiguas - Nuevas</button>
+                        <button onClick={handleASCBtn}>Nuevas - Antiguas</button>
+                        <button onClick={handleDESBtn} >Antiguas - Nuevas</button>
                         <h2>Calificacion</h2>
-                        <button>0 - 10 puntos</button>
-                        <button>10 - 0 puntos</button>
+                        <button onClick={handle0_10Btn }>0 - 10 puntos</button>
+                        <button onClick={handle10_0Btn}>10 - 0 puntos</button>
                     </div>
                 </motion.div>
                 </div>
