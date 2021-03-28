27039880db48402cd2de80a13789f11194bf3142
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-27 20:38:08 -0500

    add moviescontext to home page; add apicall

--- src/components/FilterNav/FilterNav.js
+++ src/components/FilterNav/FilterNav.js
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import Search from '../assets/imgs/Search.png'
+import Filter from '../assets/imgs/Filter.png'
+import Arrow from '../assets/imgs/Arrow.png'
+import { motion } from "framer-motion";
+import { dropdownAnimate } from '../../animations/FilterNav';
+import CheckboxInput from './CheckboxInput/CheckboxInput';
 
-const FilterNav = () => {
+const FilterNav = ({handleSubmit, genres}) => {
+    const [isHover, toggleHover] = useState(false)
+    const inputRef = useRef()
+    const toggleMenu = () => {
+        inputRef.current.focus()
+        toggleHover(!isHover)
+    }
     return (
-        <div>
-            
-        </div>
+        
+        <nav>
+            <div className="search">
+                <input type="Text" />
+                <button><img src={Search} alt="title"/></button>
+            </div>
+            <motion.div
+                className="filter"
+                onHoverStart={toggleMenu}
+                onHoverEnd={toggleMenu}
+                ref={inputRef}
+            >
+                <div>
+                    <button><img src={Filter} alt="title"/></button>
+                </div>
+                <motion.div
+                    className="menu"
+                    initial="exit"
+                    animate={isHover ? "enter" : "exit"}
+                    variants={dropdownAnimate}
+                >
+                    <div className="sub-menu-background" />
+                    <div className="sub-menu-container">
+                        <h2>Genero</h2>
+                        {genres.map(item =><CheckboxInput name={item.name}/>)}
+                    </div>
+                </motion.div>
+            </motion.div>
+            <motion.div
+                className="Sort"
+                onHoverStart={toggleMenu}
+                onHoverEnd={toggleMenu}
+                ref={inputRef}
+            >
+                <div>
+                    <p>Ordenar</p>
+                    <button> <img src={Arrow} alt="title"/></button>
+                    <motion.div
+                    className="menu"
+                    initial="exit"
+                    animate={isHover ? "enter" : "exit"}
+                    variants={dropdownAnimate}
+                >
+                    <div className="sub-menu-background" />
+                    <div className="sub-menu-container">
+                        <h2>Fecha</h2>
+                        <button>Nuevas - Antiguas</button>
+                        <button>Antiguas - Nuevas</button>
+                        <h2>Calificacion</h2>
+                        <button>0 - 10 puntos</button>
+                        <button>10 - 0 puntos</button>
+                    </div>
+                </motion.div>
+                </div>
+            </motion.div>
+
+        </nav>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { postElement } from '../apiCalls/apiCalls';
 import FilterNav from '../components/FilterNav/FilterNav';
+import { MoviesContext } from '../contextProviders/MoviesProvider';
 
 const Home = () => {
     const [query, setQuery] = useState('');
     const handleSearchquery = e => setQuery(e.target.value)
+    const [state, dispatch] = useContext(MoviesContext);
+
+    const MoviesEndpoint = `http://localhost:5000/movies`;
+
+    const getMoviesInfo = postElement
+
+    useEffect(() => {
+        getMoviesInfo({ akelab: 123456789 }, 'SHOW_MOVIES', MoviesEndpoint, dispatch);
+    }, []);
     return (
         <div>
             <FilterNav />
